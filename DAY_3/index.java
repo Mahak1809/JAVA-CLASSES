66dab060fabbf41b8936ca0a0f16fca8463cff95
Mahak Kaushal <mahakkaushal2@gmail.com>
2026-06-25 20:17:42 +0530

    Add files via upload

--- DAY_3/index.java
+++ DAY_3/index.java
@@ -1,11 +1,20 @@
-class index{
+public class index{
     public static void main(String args[]){
-        int i = 1;
-        int sum = 0;
-        while(i<=10){
-            System.out.println(sum);
-            sum+=i;
-            i++;
+        String s = "I love Java Programming";
+        int vowels = 0;
+        int consonents = 0;
+        String str = s.toLowerCase();
+        for(int i = 0 ; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch >= 'a' && ch <= 'z'){
+                if(ch=='a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                    vowels++;
+                } else {
+                    consonents++;
+                }
+            }
         }
+        System.out.println("Vowels : "+vowels);
+        System.out.println("Consonents : "+consonents);
     }
 }
